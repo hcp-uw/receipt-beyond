af22dcddf20745c6d2faf44d1955e5b720653a85
davidye007 <davidyeap@outlook.com>
2024-06-29 21:37:47 -0700

    switched from classes to using blueprints, switched from JWT to Flask-Login

--- source/backend/server_main.py
+++ source/backend/server_main.py
@@ -1,43 +1,52 @@
-from flask import Flask
-from firebase_admin import credentials, initialize_app, firestore
-from flask_restful import Api
-from resource.resource_receipt import ReceiptResource
-from resource.resource_user import Register, Login, Logout
-from resource.resource_summary import MonthlyCategoryExpenditure, MonthlySpending
-from flask_jwt_extended import JWTManager
+from flask import Flask, jsonify
+import firebase_admin
+from firebase_admin import credentials
 from config import Config
-# import logging
+from flask_login import LoginManager
+from flask_login import LoginManager, login_required, UserMixin, login_user, logout_user, current_user
+from google.cloud import firestore
+import firebase_admin
+from firebase_admin import firestore as admin_firestore
+from firebase_admin import credentials, initialize_app, firestore
+from model.user import User
+# Initialize LoginManager
+login_manager = LoginManager()
 
+def create_app():
 
-# Initialize flask application and API
-app = Flask(__name__)
-app.config.from_object(Config)
-api = Api(app, prefix='/api')
+  # App config
+  app = Flask(__name__)
+  app.config.from_object(Config)
 
-# logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
-# app.logger.setLevel(logging.INFO)
+  # Initialize firebase admin SDK app
+  cred = credentials.Certificate(app.config['FIRESTORE_KEY'])
+  firebase_admin.initialize_app(cred)
+  db = firestore.client()
 
-# Initialize jwt manager
-jwt = JWTManager(app)
+  # Flask-Login config
+  login_manager.init_app(app)
+  login_manager.login_view = 'auth.login'
 
-# Define user identity loader
-@jwt.user_identity_loader
-def user_identity_lookup(user_id):
-    return user_id
+  @login_manager.user_loader
+  def load_user(user_id):
+      print(user_id)
+      user_ref = db.collection('Users').document(user_id).get()
+      if user_ref.exists:
+          user = User(user_id)
+          return user
+      return None
 
-# Initialize Firestore database and Firestore client
-cred = credentials.Certificate(app.config['FIRESTORE_KEY'])
-initialize_app(cred)
-db = firestore.client()
+  # Import and register blueprints
+  from resource.resource_user import auth_bp
+  from resource.resource_receipt import receipts_bp
+  from resource.resource_summary import summary_bp
+  app.register_blueprint(auth_bp, url_prefix='/api')
+  app.register_blueprint(receipts_bp, url_prefix='/api')
+  app.register_blueprint(summary_bp, url_prefix='/api')
 
-# Add resources to API
-api.add_resource(ReceiptResource, '/receipt', resource_class_args=(db,))
-api.add_resource(Register, '/register', resource_class_args=(db,))
-api.add_resource(Login, '/login', resource_class_args=(db,))
-api.add_resource(Logout, '/logout', resource_class_args=(db,))
-api.add_resource(MonthlyCategoryExpenditure, '/month_cat_exp', resource_class_args=(db,))
-api.add_resource(MonthlySpending, '/month_exp', resource_class_args=(db,))
+  return app
 
 # Run app
 if __name__ == '__main__':
+  app = create_app()
   app.run(debug=True)
