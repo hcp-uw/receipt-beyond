637fd2168374c29930a8989e766de0d2b9adc812
jhyoo725 <jhyoo725@cs.washington.edu>
2024-09-04 00:23:43 -0700

    first setup

--- source/frontend/pages/summary.tsx
+++ source/frontend/pages/summary.tsx
@@ -1,15 +1,30 @@
 import { Text, View } from "react-native";
+import React, {Component} from "react";
 
-export default function Summary() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-    <Text>Summary Screen</Text>
-    </View>
-  );
+interface SummaryState {
+  date: string
+}
+
+
+export class Summary extends Component<{}, SummaryState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      date: ""
+    }
+  }
+
+  render = (): JSX.Element => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+      <Text>Summary Screen</Text>
+      </View>
+    );
+  }
 }
