d3a4a24ab2b635e608eaf889be8efe1e4e918bc8
jhyoo725 <jhyoo725@cs.washington.edu>
2024-09-06 15:06:00 -0700

    setup for summary

--- source/frontend/pages/summary.tsx
+++ source/frontend/pages/summary.tsx
@@ -1,18 +1,34 @@
 import { Text, View } from "react-native";
 import React, {Component} from "react";
+import { BarChart, PieChart } from 'react-native-chart-kit';
+import { NavigationProp, RouteProp } from "@react-navigation/native";
+import { SummaryStackParamList } from "../app/StackParamList";
 
-interface SummaryState {
-  date: string
+interface SummaryProps {
+  navigation: NavigationProp<SummaryStackParamList, "Summary">;
+  route: RouteProp<SummaryStackParamList, "Summary">;
 }
 
+interface SummaryState {
+  date: string,
+  pieData: any[]
+}
 
-export class Summary extends Component<{}, SummaryState> {
-  constructor(props: {}) {
+export class Summary extends Component<SummaryProps, SummaryState> {
+  constructor(props: SummaryProps) {
     super(props);
     this.state = {
-      date: ""
-    }
-  }
+      date: "",
+      pieData: []
+    };
+  };
+
+  componentDidMount(): void {
+    //TODO: Test if the start of the page considered as a change of the screen
+    this.props.navigation.addListener("focus", () => {
+      this.fetchData();
+    });
+  };
 
   render = (): JSX.Element => {
     return (
@@ -26,5 +42,27 @@ export class Summary extends Component<{}, SummaryState> {
       <Text>Summary Screen</Text>
       </View>
     );
-  }
+  };
+
+  // Need to give a date as a json but method is GET in backend.
+  fetchData = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const currDate = `${year}-${month}-${day}`;
+
+    this.setState({date: currDate});
+
+    const args = {date: currDate};
+    fetch("https://receiptplus.pythonanywhere.com/api/month_cat_exp", {
+      method: "POST", body: JSON.stringify(args),
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    })
+    .then(this.handleChange)
+    .catch((error) => {
+      console.error("Error fetching api/user_info");
+    });
+  };
 }
