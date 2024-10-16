9b8f64d9ffce58fb746bfa5168b17f4131f7268d
davye <davye@uw.edu>
2024-10-16 15:15:27 -0700

    correct error handling messages, delete extra space at end of fetch url

--- source/frontend/pages/summary.tsx
+++ source/frontend/pages/summary.tsx
@@ -1,16 +1,22 @@
 import { Text, View, Dimensions, ScrollView } from "react-native";
 import React, { Component } from "react";
-import {PieChart } from "react-native-chart-kit";
-import {VictoryChart, VictoryLine, VictoryTheme, VictoryLabel, VictoryAxis} from "victory-native";
+import { PieChart } from "react-native-chart-kit";
+import {
+  VictoryChart,
+  VictoryLine,
+  VictoryTheme,
+  VictoryLabel,
+  VictoryAxis,
+} from "victory-native";
 import { NavigationProp, RouteProp } from "@react-navigation/native";
 import { SummaryStackParamList } from "../app/StackParamList";
 
 // TODO:
 /**
- * 1. Fix the Scale of the height. This can be done by finding the maxY (highest amount) 
+ * 1. Fix the Scale of the height. This can be done by finding the maxY (highest amount)
  *    and setting the domain of the y-axis in VictoryChart (domain={{ y: [0, maxY * 1.1] }})
  * 2. Make the horizontal scoll where the graph moves based on the scroll
- *    ***Idea: 
+ *    ***Idea:
  *            1. Set the scale of the graph shown to be about 10 days and scroll through
  *            2. Make a button or tab when it is pressed, it shows the entire Day 1 ~ Curr Date View
  *               Ex) Google Stock graph view
@@ -50,7 +56,7 @@ export class Summary extends Component<SummaryProps, SummaryState> {
       lineData: [],
       maxY: 0,
       loading: true,
-      totalAmount: 0
+      totalAmount: 0,
     };
   }
 
@@ -64,7 +70,7 @@ export class Summary extends Component<SummaryProps, SummaryState> {
     Dining: "#FF9800", // Orange
     Gas: "#2196F3", // Blue
     Shopping: "#9C27B0", // Purple
-    Food: "#FF4545", // Red 
+    Food: "#FF4545", // Red
   };
 
   componentDidMount(): void {
@@ -77,8 +83,12 @@ export class Summary extends Component<SummaryProps, SummaryState> {
   }
 
   render = (): JSX.Element => {
-    if (this.state.loading || this.state.pieData.length == 0 || this.state.lineData.length == 0) {
-      return <View></View>
+    if (
+      this.state.loading ||
+      this.state.pieData.length == 0 ||
+      this.state.lineData.length == 0
+    ) {
+      return <View></View>;
     }
 
     // return (
@@ -175,7 +185,7 @@ export class Summary extends Component<SummaryProps, SummaryState> {
           <View
             style={{
               alignItems: "center",
-              marginLeft: -20
+              marginLeft: -20,
             }}
           >
             <Text
@@ -192,39 +202,41 @@ export class Summary extends Component<SummaryProps, SummaryState> {
             <VictoryChart
               width={Dimensions.get("window").width - 20}
               height={300}
-              scale={{x: "linear", y: "linear"}}
+              scale={{ x: "linear", y: "linear" }}
               theme={VictoryTheme.material}
-              padding={{ top: 50, bottom: 50, left:70, right: 20 }}
-              domain={{x: [1, this.state.lineData.length], y: [0, this.state.maxY * 1.1]}}
-              domainPadding={{y:10}}
+              padding={{ top: 50, bottom: 50, left: 70, right: 20 }}
+              domain={{
+                x: [1, this.state.lineData.length],
+                y: [0, this.state.maxY * 1.1],
+              }}
+              domainPadding={{ y: 10 }}
             >
               <VictoryAxis
                 label="DAYS"
                 style={{
-                  axis: {stroke: "#756f6a"},
-                  axisLabel: {padding: 30, fontSize: 16},
-                  tickLabels: {fontSize: 12}
+                  axis: { stroke: "#756f6a" },
+                  axisLabel: { padding: 30, fontSize: 16 },
+                  tickLabels: { fontSize: 12 },
                 }}
               />
               <VictoryAxis
                 dependentAxis
                 label="AMOUNTS"
                 style={{
-                  axis: {stroke: "#756f6a"},
-                  axisLabel: {padding: 40, fontSize: 16},
-                  tickLabels: {fontSize: 12}
+                  axis: { stroke: "#756f6a" },
+                  axisLabel: { padding: 40, fontSize: 16 },
+                  tickLabels: { fontSize: 12 },
                 }}
               />
               <VictoryLine
                 data={this.state.lineData}
                 labels={({ datum }) => datum.y}
-                labelComponent={<VictoryLabel renderInPortal dy={-20}/>}
+                labelComponent={<VictoryLabel renderInPortal dy={-20} />}
                 style={{
                   data: { stroke: "#c43a31" },
-                  parent: { border: "1px solid #ccc"}
+                  parent: { border: "1px solid #ccc" },
                 }}
-              >
-              </VictoryLine>
+              ></VictoryLine>
             </VictoryChart>
           </View>
         </InnerStyledContainer>
@@ -234,7 +246,7 @@ export class Summary extends Component<SummaryProps, SummaryState> {
 
   // Need to give a date as a json but method is GET in backend.
   fetchData = () => {
-    console.log("fetchData")
+    console.log("fetchData");
     const date = new Date();
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
@@ -246,31 +258,31 @@ export class Summary extends Component<SummaryProps, SummaryState> {
 
     // const args = { date: currDate };
     // For testing
-    const args = {date: '2024-10-30'};
+    const args = { date: "2024-10-30" };
     fetch("https://receiptplus.pythonanywhere.com/api/month_cat_exp", {
       method: "POST",
       body: JSON.stringify(args),
       headers: { "Content-Type": "application/json" },
       credentials: "include",
     })
-    .then((res) => this.handleChange(res, "pie"))
-    .catch((error) => {
-      console.error("Error fetching api/user_info");
-    });
+      .then((res) => this.handleChange(res, "pie"))
+      .catch((error) => {
+        console.error("Error fetching api/month_cat_exp");
+      });
 
-    fetch("https://receiptplus.pythonanywhere.com/api/month_exp ", {
+    fetch("https://receiptplus.pythonanywhere.com/api/month_exp", {
       method: "POST",
       body: JSON.stringify(args),
       headers: { "Content-Type": "application/json" },
       credentials: "include",
     })
-    .then((res) => this.handleChange(res, "line"))
-    .catch((error) => {
-      console.error("Error fetching api/user_info");
-    });
+      .then((res) => this.handleChange(res, "line"))
+      .catch((error) => {
+        console.error("Error fetching api/month_exp");
+      });
   };
 
-  handleChange = (res: Response, graph:string) => {
+  handleChange = (res: Response, graph: string) => {
     if (res.ok) {
       res.json().then((data) => {
         if (graph === "pie") {
@@ -300,17 +312,22 @@ export class Summary extends Component<SummaryProps, SummaryState> {
     }));
 
     // Update state
-    this.setState({ pieData: newData, loading: false});
+    this.setState({ pieData: newData, loading: false });
   };
 
-  processlineData = (data: {x:number, y:number}[]) => {
-    const validY = data.map(point=>point.y).filter(y => y != null);
+  processlineData = (data: { x: number; y: number }[]) => {
+    const validY = data.map((point) => point.y).filter((y) => y != null);
     const max = Math.max(...validY);
-  
+
     const total = data.reduce((sum, point) => sum + point.y, 0);
 
-    this.setState({lineData: data, loading: false, maxY: max, totalAmount: total});
-  }
+    this.setState({
+      lineData: data,
+      loading: false,
+      maxY: max,
+      totalAmount: total,
+    });
+  };
 
   getRandomColor = () => {
     const letters = "0123456789ABCDEF";
